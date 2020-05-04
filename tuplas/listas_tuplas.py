0ad868605a1ffb47ca618dc9481b6e192c9d88f5
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-04 13:36:43 -0500

    De listas a tuplas

---
+++ tuplas/listas_tuplas.py
@@ -0,0 +1,20 @@
+lista = ["Curso","Python","Py"]
+tupla = ("Introducción","Básico","Listas","Tuplas")
+mensaje = "Hola Mundo"
+
+#list regresa nueva lista
+nueva_lista = list(tupla)
+
+print(nueva_lista)
+
+#tuple
+nueva_tupla = tuple(lista)
+
+print(nueva_tupla)
+
+#
+nueva_lista = list(mensaje)
+nueva_tupla = tuple(mensaje)
+
+print(nueva_lista)
+print(nueva_tupla)
