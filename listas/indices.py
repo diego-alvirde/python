03252279a3981b9d549f6de4e7f5c23788f3706e
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-04 11:53:54 -0500

    Índices y Sublistas

---
+++ listas/indices.py
@@ -0,0 +1,12 @@
+cursos = ["python", "django", "flask", "c" , "php", "java"]
+print(cursos[0])
+#[desde:hasta]
+#sublista_cursos = cursos[0:3]
+#sublista_cursos = cursos[:3]
+#sublista_cursos = cursos[2:4]
+#sublista_cursos = cursos[:]
+#[desde,hasta,salto]
+#sublista_cursos = cursos[0:7:2]
+#[::-1] imprimir el inverso de nuestra lista
+sublista_cursos = cursos[::-1]
+print(sublista_cursos)
