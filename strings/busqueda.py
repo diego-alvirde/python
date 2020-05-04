8961921c48c15dbce9a002659a164f5a8b543191
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-04 14:42:21 -0500

    Búsqueda de cadenas

---
+++ strings/busqueda.py
@@ -0,0 +1,11 @@
+mensaje = "Hola mundo desde python"
+#resultado = mensaje.count("desde")
+#resultado = "desde" in mensaje
+#El metodo find nos regresa un -1 si no encuentra la cadena
+#resultado = mensaje.find("desde")
+#resultado = mensaje[resultado:resultado+len("desde")]
+#startswith palabra al inicio
+#resultado = mensaje.startswith("Hola")
+#endswith palabra al final
+resultado = mensaje.endswith("hon")
+print(resultado)
