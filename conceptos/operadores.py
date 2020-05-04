86da6a291a8f57fac913368925c10929ad8059e6
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-04 10:53:15 -0500

    Operadores relacionales y lógicos

---
+++ conceptos/operadores.py
@@ -0,0 +1,21 @@
+uno = 10
+dos = 18
+
+mayor = uno > dos
+menor = uno < dos
+mayor_igual = uno >= dos
+menor_igual = uno <= dos
+igual = uno is dos
+diferente = uno != dos
+
+#resultado = True and diferente
+resultado = True or diferente
+
+print(mayor)
+print(menor)
+print(mayor_igual)
+print(menor_igual)
+print(igual)
+print(diferente)
+print(resultado)
+
