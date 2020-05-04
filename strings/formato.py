ea9bf94aafbe637be79b54a34cf23abdfe594ddb
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-04 14:18:51 -0500

    Formato para cadenas

---
+++ strings/formato.py
@@ -0,0 +1,18 @@
+texto = "curso de python 3"
+#Capitalize primera letra en mayuscula
+#resultado = texto.capitalize()
+#swapCase todas las mayusculas a minusculas y viceversa
+#resultado = texto.swapcase()
+#upper todas las letras a mayusculas
+#resultado = texto.upper()
+#lower todas las letras a minusculas
+#resultado = texto.lower()
+#title genera texto en forma de titulo
+#resultado = texto.title()
+#replace reemplaza una palabra por otra
+#resultado = texto.replace("python","php")
+#strip elimina espacios al inicio y final de una cadena
+resultado = texto.strip()
+print(texto.isupper())
+print(texto.islower())
+print(resultado)
