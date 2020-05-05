f85e20bb94388a9c0ea169ccc53eebe0f1924c7e
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-05 12:32:21 -0500

    break y continue

---
+++ ciclos_condicionales/break_continue.py
@@ -0,0 +1,11 @@
+titulo = "Python 3"
+
+for caracter in titulo:
+  if caracter == "3":
+    break
+  print(caracter)
+
+for caracter in titulo:
+  if caracter == "3":
+    continue
+  print(caracter)
