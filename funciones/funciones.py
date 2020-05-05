1d2848f1a3da1486f4f56795983c962674ec1bf8
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-05 14:53:58 -0500

    Múltiples valores de entrada y salida

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,4 +1,17 @@
-def saluda():
-  print("Función creada con python")
+def crear_mensaje(nombre):
+  return "Hola {}, bienvenido al curso de python 3".format(nombre)
 
-saluda()
+def suma(a,b,c):
+  return a+b+c
+
+def obtener_curso():
+  return "Curso de Python", "Básico", 3.6
+
+mensaje = crear_mensaje("Diego Armando Maradona")
+print(mensaje)
+print(suma(12,32,63))
+
+curso, nivel, version = obtener_curso()
+print(curso)
+print(nivel)
+print(version)
