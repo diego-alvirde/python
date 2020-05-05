3ed636002513b3191674bb057c0bb089f90fd94e
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-05 12:02:49 -0500

    Ciclo for

---
+++ ciclos_condicionales/for.py
@@ -0,0 +1,11 @@
+numeros = [1,2,3,4,5,6,7,8,9,10]
+diccionario = {'a':1,'b':2}
+
+#for numero in numeros:
+for llave in diccionario:
+  print(llave)
+
+valores = ( (10,20), ["a","b"], (True,False) )
+
+for valor1, valor2 in valores:
+  print(valor1, valor2)
