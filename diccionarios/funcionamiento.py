37ae14eb5fd7ebd0ca759b44d9a535eb1ec44b45
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-05 10:54:34 -0500

    Cómo funcionan los diccionarios

---
+++ diccionarios/funcionamiento.py
@@ -0,0 +1,13 @@
+diccionario = {}
+#Para agregar una llave con su valor
+diccionario["nombre"] = "Milan"
+#Obtener valor de la llave
+#valor = diccionario["nombre"]
+#Modificar valor de la llave
+diccionario["nombre"] = 90
+valor = diccionario["nombre"]
+print(diccionario)
+print(valor)
+
+n_diccionario = {"a":1,"b":2,"c":3,"a":4}
+print(n_diccionario)
