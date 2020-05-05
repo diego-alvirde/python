7931b5d58ba12966ac2dbbcb1ce8e0f5cc4de50e
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-05 11:07:50 -0500

    Llaves, ítems y valores

---
+++ diccionarios/llaves.py
@@ -0,0 +1,9 @@
+diccionario = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8}
+
+llaves = diccionario.keys()
+valor = diccionario.values()
+items = diccionario.items()
+#resultado = list(resultado)
+print(llaves)
+print(valor)
+print(items)
