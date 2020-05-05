1b3a688b8ec12d919777d2b4604f61273e9fe46e
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-05 11:45:04 -0500

    Condicionales

---
+++ ciclos_condicionales/condicionales.py
@@ -0,0 +1,16 @@
+#False, "", '', [], {}, 0, 0.0
+color = "rojo"
+
+if color == "verde":
+  print("Puede continuar")
+elif color == "rojo":
+  print("Alto parcial")
+else:
+  print("Alto total")
+
+variable = True
+
+if variable:
+  print("True")
+else:
+  print("False")
