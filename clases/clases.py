b2ba6873ab95f1bdf46366bab8a781cfee05e3ad
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-06 14:09:40 -0500

    Atributos

--- clases/clases.py
+++ clases/clases.py
@@ -1,9 +1,24 @@
 class Usuario:
-  def saluda(self, nombre):
-    return "Función saludar" + nombre
 
-objeto = Usuario()
+  def __init__(self, username='',correo='',nombre=''):
+    self.username = username
+    self.correo = correo
+    self.nombre = nombre
+
+  def saluda(self):
+    return "Función saludar" + self.nombre
+
+  def mostrar_username(self):
+    print(self.username)
+
+  def mostrar_nombre(self):
+    print(self.nombre)
+
+objeto = Usuario("Username","correo@mail.com","Nombre")
 
 #Saber de que tipo es un objeto
 print(type(objeto))
-print(objeto.saluda("Diego"))
+print(objeto.saluda())
+objeto.mostrar_username()
+
+objeto.mostrar_nombre()
