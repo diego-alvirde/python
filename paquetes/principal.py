bfd50fe7124ddb2eb8a0791b922a60b9be0a8747
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-07 10:36:00 -0500

    Formas de import

--- paquetes/principal.py
+++ paquetes/principal.py
@@ -1,6 +1,8 @@
-import calculadora
+#import calculadora
+#from calculadora import *
+from calculadora import suma as n_suma, resta as n_resta, multiplicacion as n_multiplicacion, division as n_division
 
-print(calculadora.suma(1,2))
-print(calculadora.resta(12,2))
-print(calculadora.multiplicacion(1,2))
-print(calculadora.division(1,2))
+print(n_suma(1,2))
+print(n_resta(12,2))
+print(n_multiplicacion(1,2))
+print(n_division(1,2))
