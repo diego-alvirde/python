5bf12c01fa9085354579c59491c6a05131cb3d8f
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-05-07 10:56:50 -0500

    Atributo __name__

--- paquetes/calculadora.py
+++ paquetes/calculadora.py
@@ -9,3 +9,9 @@ def multiplicacion(a,b):
 
 def division(a,b):
   return a/b
+
+print(__name__)
+if __name__ == "__main__":
+  print("Mensaje desde calculadora")
+else:
+  print("Estoy siendo usado como un modulo")
--- paquetes/principal.py
+++ paquetes/principal.py
@@ -1,8 +1,11 @@
 #import calculadora
 #from calculadora import *
-from calculadora import suma as n_suma, resta as n_resta, multiplicacion as n_multiplicacion, division as n_division
-
+#from calculadora import suma as n_suma, resta as n_resta, multiplicacion as n_multiplicacion, division as n_division
+import calculadora
+"""
 print(n_suma(1,2))
 print(n_resta(12,2))
 print(n_multiplicacion(1,2))
 print(n_division(1,2))
+"""
+print(calculadora.__name__)
